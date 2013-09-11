0a0698587e25ba1f60bf0a87d9f02ee75b96475b
Laszlo Dobos <dobos@complex.elte.hu>
2013-09-11 17:04:41 -0400

    Added self-join test, but not implemented self-join yet.

--- test/Jhu.SkyQuery.Test/XMatchQueryTest.cs
+++ test/Jhu.SkyQuery.Test/XMatchQueryTest.cs
@@ -106,6 +106,35 @@ WHERE     p.RA BETWEEN 0 AND 5 AND p.Dec BETWEEN 0 AND 5
         }
 
         [TestMethod]
+        public void SelfJoinTest()
+        {
+            var sql =
+@"SELECT s.objid, s.ra, s.dec, g.objid, g.ra, g.dec, x.ra, x.dec
+INTO [$targettable]
+FROM SDSSDR7:PhotoObjAll AS s
+CROSS JOIN SDSSDR7:PhotoObjAll AS g
+XMATCH BAYESFACTOR x
+MUST EXIST s ON POINT(s.cx, s.cy, s.cz), 0.1, 0.1, 0.1
+MUST EXIST g ON POINT(g.cx, g.cy, g.cz), 0.2, 0.2, 0.2
+HAVING LIMIT 1e3
+WHERE s.ra BETWEEN 0 AND 5 AND s.dec BETWEEN 0 AND 5
+	AND g.ra BETWEEN 0 AND 5 AND g.dec BETWEEN 0 AND 5";
+
+            using (SchedulerTester.Instance.GetExclusiveToken())
+            {
+                SchedulerTester.Instance.EnsureRunning();
+                var guid = ScheduleQueryJob(
+                                    sql.Replace("[$targettable]", "XMatchQueryTest_SelfJoinTest"),
+                                    QueueType.Long);
+
+                WaitJobComplete(guid, TimeSpan.FromSeconds(10));
+
+                var ji = LoadJob(guid);
+                Assert.AreEqual(JobExecutionState.Completed, ji.JobExecutionStatus);
+            }
+        }
+
+        [TestMethod]
         public void CartesianCoordinatesTest()
         {
             var sql =
