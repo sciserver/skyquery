e04744f6f3a5c587dc46535ebbcff7db41e844e0
Laszlo Dobos <dobos@complex.elte.hu>
2013-11-03 16:51:23 +0100

    Fits tests added.

--- test/Jhu.SkyQuery.Format.Test/Format/Fits/ImageHduTest.cs
+++ test/Jhu.SkyQuery.Format.Test/Format/Fits/ImageHduTest.cs
@@ -3,9 +3,8 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Jhu.SharpFitsIO;
 
-namespace Jhu.SharpFitsIO.Test
+namespace Jhu.SkyQuery.Format.Fits.Test
 {
     [TestClass]
     public class ImageHduTest
