50fff217cb82ded56696c007afd5149413e0a96a
Laszlo Dobos <dobos@complex.elte.hu>
2013-11-03 16:51:08 +0100

    Added SkyQuery file formats to FileFormatFactory.

---
+++ dll/Jhu.SkyQuery.Format/Format/SkyQueryFileFormatFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.IO;
+using Jhu.Graywulf.Format;
+
+namespace Jhu.SkyQuery.Format
+{
+    public class SkyQueryFileFormatFactory : Jhu.Graywulf.Format.FileFormatFactory
+    {
+        public SkyQueryFileFormatFactory()
+        {
+        }
+
+        protected override void OnCreateFileFormatDescriptions(HashSet<Type> fileTypes)
+        {
+            base.OnCreateFileFormatDescriptions(fileTypes);
+
+            fileTypes.Add(typeof(VOTable.VOTable));
+            fileTypes.Add(typeof(Fits.Fits));
+        }
+    }
+}
