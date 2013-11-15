112ec7c3bdb930f73cebbdaf120759d67a9aa71f
Laszlo Dobos <dobos@complex.elte.hu>
2013-11-15 09:44:13 -0500

    Added PK to USNOB PhotoObj table script.

--- sql/SkyNodes/SkyNode_USNOB.sql
+++ sql/SkyNodes/SkyNode_USNOB.sql
@@ -70,6 +70,14 @@ CREATE TABLE [dbo].[PhotoObj](
 ) ON [PRIMARY]
 GO
 
+ALTER TABLE [dbo].[PhotoObj] ADD PRIMARY KEY CLUSTERED 
+(
+	[objid] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+GO
+
+
+
 
 -- Index to support on the fly zone table creation
 CREATE NONCLUSTERED INDEX [IX_PhotoObj_Zone] ON [dbo].[PhotoObj] 
