125abf424de44865288e5e9783106de73cb4b554
Laszlo Dobos <dobos@complex.elte.hu>
2013-12-27 10:29:26 +0100

    SkyQuery test config changes.

---
+++ test/Jhu.SkyQuery.Test/AppSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Jhu.SkyQuery.Test
+{
+    public static class AppSettings
+    {
+        public static string SkyQueryTestConnectionString
+        {
+            get
+            {
+                var cs = ConfigurationManager.ConnectionStrings["Jhu.SkyQuery..Test"];
+                return cs != null ? cs.ConnectionString : null;
+            }
+        }
+    }
+}
