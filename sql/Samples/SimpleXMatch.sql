9a3339c98558b390184966d14b16d6708581931f
Laszlo Dobos <dobos@complex.elte.hu>
2014-03-12 16:20:08 -0400

    Sample query updated.

--- sql/Samples/SimpleXMatch.sql
+++ sql/Samples/SimpleXMatch.sql
@@ -1,10 +1,10 @@
 SELECT s.objid, s.ra, s.dec, g.objid, g.ra, g.dec, x.ra, x.dec
 INTO clxmatch
-FROM SDSSDR7:PhotoObjAll AS s
-CROSS JOIN GALEX:PhotoObjAll AS g
+FROM SDSSDR7:PhotoObjAll AS s WITH(POINT(s.ra, s.dec), ERROR(0.1, 0.1, 0.1))
+CROSS JOIN GALEX:PhotoObjAll AS g WITH(POINT(g.ra, g.dec), ERROR(0.2, 0.2, 0.2))
 XMATCH BAYESFACTOR x
-MUST EXIST s ON POINT(s.ra, s.dec), 0.1, 0.1, 0.1
-MUST EXIST g ON POINT(g.ra, g.dec), 0.2, 0.2, 0.2
+MUST EXIST s
+MUST EXIST g
 HAVING LIMIT 1e3
 WHERE s.ra BETWEEN 0 AND 5 AND s.dec BETWEEN 0 AND 5
     AND g.ra BETWEEN 0 AND 5 AND g.dec BETWEEN 0 AND 5
