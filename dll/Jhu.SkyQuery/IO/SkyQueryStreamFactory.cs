495c6b9376835f080fd47c38cbddc72e8fa3ac8e
Laszlo Dobos <dobos@complex.elte.hu>
2014-06-21 17:40:10 +0200

    Merged IO with base SkyQuery lib. File formaters are being moved to separate project with no dependency on Graywulf or SkyQuery.

---
+++ dll/Jhu.SkyQuery/IO/SkyQueryStreamFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jhu.Graywulf.IO;
+
+namespace Jhu.SkyQuery.IO
+{
+    public class SkyQueryStreamFactory : StreamFactory
+    {
+
+    }
+}
