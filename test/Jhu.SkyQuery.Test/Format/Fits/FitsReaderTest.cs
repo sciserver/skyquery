643c22165db68bc8bdc2a69aa0207ce5d939f0a0
Laszlo Dobos <dobos@complex.elte.hu>
2014-06-22 13:07:20 +0200

    Removed all format test project. Added tests to main SkyQuery lib.

--- test/Jhu.SkyQuery.Test/Format/Fits/FitsReaderTest.cs
+++ test/Jhu.SkyQuery.Test/Format/Fits/FitsReaderTest.cs
@@ -3,6 +3,8 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jhu.Graywulf.IO;
+using Jhu.Graywulf.Format;
 using Jhu.SkyQuery.Format.Fits;
 
 namespace Jhu.SkyQuery.Format.Fits.Test
@@ -10,12 +12,11 @@ namespace Jhu.SkyQuery.Format.Fits.Test
     [TestClass]
     public class FitsReaderTest
     {
-        private FitsFile OpenFits(string path)
+        private FitsFileWrapper OpenFits(string path)
         {
-            var f = new FitsFile(
+            var f = new FitsFileWrapper(
                 new Uri(String.Format("../../../test/files/{0}", path), UriKind.Relative),
-                FitsFileMode.Read,
-                Graywulf.Schema.Endianness.BigEndian
+                DataFileMode.Read
                 );
 
             return f;
@@ -29,10 +30,6 @@ namespace Jhu.SkyQuery.Format.Fits.Test
 
             using (var dr = cmd.ExecuteReader())
             {
-                Assert.AreEqual(0, dr.FieldCount);
-
-                dr.NextResult();
-
                 Assert.AreEqual(23, dr.FieldCount);
 
                 var values = new object[dr.FieldCount];
