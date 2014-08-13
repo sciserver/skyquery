945d8e0f4d886cb8ac9724cee36d004928574531
Laszlo Dobos <dobos@complex.elte.hu>
2014-08-13 16:13:32 -0400

    Factored out IO classes into separate library.

---
+++ dll/Jhu.SkyQuery.IO/Copyright.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jhu.SkyQuery
+{
+    static class Copyright
+    {
+#if DEBUG
+        public const string InfoConfiguration = "Debug";
+#else
+        public const string InfoConfiguration = "Release";
+#endif
+        public const string InfoProduct = "SkyQuery";
+        public const string InfoCompany = "ELTE/JHU IDIES";
+        public const string InfoCopyright = "Copyright ©  2008-2014 László Dobos, Eötvös University, The Johns Hopkins University";
+        public const string InfoTrademark = "";
+        public const string InfoCulture = "";
+    }
+}
