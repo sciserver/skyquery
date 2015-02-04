7bed50ee76df2967f9a53bcc46f7fc96cdafea36
Laszlo Dobos <dobos@complex.elte.hu>
2015-02-04 16:58:13 -0500

    Metadata updated to new format.

--- sql/SkyNodes/SkyNode_First.sql
+++ sql/SkyNodes/SkyNode_First.sql
@@ -1,27 +1,98 @@
 USE [SkyNode_First]
 GO
 
-CREATE TABLE [dbo].[PhotoObj](
 --/ <summary>The main PhotoObj table for the First catalog</summary>
 --/ <remarks>The main PhotoObj table for the First catalog</remarks>
-	[objID] [bigint] NOT NULL, --/ <column>unique object identifier</column>
-	[ra] [float] NOT NULL, --/ <column unit="deg">J2000 right ascension</column>
-	[dec] [float] NOT NULL, --/ <column unit="deg">J2000 declination</column>
-	[w] [char](1) NOT NULL, --/ <column>W warning flag</column>
-	[fpeak] [real] NOT NULL, --/ <column unit="mJy">peak flux density</column>
-	[fint] [real] NOT NULL, --/ <column unit="mJy">integrated flux density</column>
-	[rms] [real] NOT NULL, --/ <column unit="mJy">local rms noise estimate</column>
-	[maj] [real] NOT NULL, --/ <column unit="arcsec">major axis raw</column>
-	[min] [real] NOT NULL, --/ <column unit="arcsec">minor axis raw</column>
-	[pa] [real] NOT NULL, --/ <column unit="deg">position angle raw</column>
-	[fmaj] [real] NOT NULL, --/ <column unit="arcsec">major axis (fitted)</column>
-	[fmin] [real] NOT NULL, --/ <column unit="arcsec">minor axis (fitted)</column>
-	[fpa] [real] NOT NULL, --/ <column unit="deg">position angle (fitted)</column>
-	[fieldname] [varchar](32) NOT NULL, --/ <column>name of the coadded image containing the source</column>
-  [htmid] [bigint] NOT NULL, --/ <column>htmid for spatial searches</column>
-  [cx] [float] NOT NULL, --/ <column>cartesian x coordinate</column>
-	[cy] [float] NOT NULL, --/ <column>cartesian x coordinate</column>
-	[cz] [float] NOT NULL, --/ <column>cartesian x coordinate</column>
+CREATE TABLE [dbo].[PhotoObj](
+
+	--/ <summary>unique object identifier</summary>
+	--/ <quantity>meta.id</quantity>
+	--/ <class>id</class>
+	[objID] [bigint] NOT NULL, 
+
+	--/ <summary>J2000 right ascension</summary>
+	--/ <quantity>pos.eq.ra</quantity>
+	--/ <unit>deg</unit>
+	--/ <class>point.ra</class>
+	[ra] [float] NOT NULL, 
+
+	--/ <summary>J2000 declination</summary>
+	--/ <quantity>pos.eq.dec</quantity>
+	--/ <unit>deg</unit>
+	--/ <class>point.dec</class>
+	[dec] [float] NOT NULL, 
+
+	--/ <summary>W warning flag</summary>
+	--/ <quantity></quantity>
+	[w] [char](1) NOT NULL, 
+
+	--/ <summary>peak flux density</summary>
+	--/ <quantity>em.radio, stat.max</quantity>
+	--/ <unit>mJy</unit>
+	[fpeak] [real] NOT NULL,
+	
+	--/ <summary>integrated flux density</summary> 
+	--/ <quantity>em.radio</quantity>
+	--/ <unit>mJy</unit>
+	[fint] [real] NOT NULL,
+	
+	--/ <summary>local rms noise estimate</summary> 
+	--/ <quantity>em.radio, stat.error</quantity>
+	--/ <unit>mJy</unit>
+	[rms] [real] NOT NULL, 
+
+	--/ <summary>major axis raw</summary>
+	--/ <quantity>phys.size.smajAxis</quantity>
+	--/ <unit>arcsec</unit>
+	[maj] [real] NOT NULL, 
+
+	--/ <summary>minor axis raw</summary>
+	--/ <quantity>phys.size.sminAxis</quantity>
+	--/ <unit>arcsec</unit>
+	[min] [real] NOT NULL,
+	
+	--/ <summary>position angle raw</summary> 
+	--/ <quantity>pos.posAng</quantity>
+	--/ <unit>deg</unit>
+	[pa] [real] NOT NULL, 
+
+	--/ <summary>major axis (fitted)</summary>
+	--/ <quantity>phys.size.smajAxis, stat.fit</quantity>
+	--/ <unit>arcsec</unit>
+	[fmaj] [real] NOT NULL, 
+
+	--/ <summary>minor axis (fitted)</summary>
+	--/ <quantity>phys.size.sminAxis, stat.fit</quantity>
+	--/ <unit>arcsec</unit>
+	[fmin] [real] NOT NULL,
+	
+	--/ <summary>position angle (fitted)</summary> 
+	--/ <quantity>pos.posAng, stat.fit</quantity>
+	--/ <unit>arcsec</unit>
+	[fpa] [real] NOT NULL, 
+
+	--/ <summary>name of the coadded image containing the source</summary>
+	[fieldname] [varchar](32) NOT NULL, 
+
+	--/ <summary>htmid for spatial searches</summary>
+	--/ <quantity>pos, meta.id</quantity>
+	--/ <class>point.htmid</class>
+    [htmid] [bigint] NOT NULL, 
+
+	--/ <summary>cartesian x coordinate</summary>
+	--/ <quantity>pos.cartesian.x</quantity>
+	--/ <class>point.x</class>
+    [cx] [float] NOT NULL, 
+
+	--/ <summary>cartesian x coordinate</summary>
+	--/ <quantity>pos.cartesian.y</quantity>
+	--/ <class>point.y</class>
+	[cy] [float] NOT NULL, 
+
+	--/ <summary>cartesian x coordinate</summary>
+	--/ <quantity>pos.cartesian.z</quantity>
+	--/ <class>point.z</class>
+	[cz] [float] NOT NULL, 
 ) ON [PRIMARY]
 
 
