c5e804f078761c5c50514f7d3bb8f0332ddafef4
Laszlo Dobos <dobos@complex.elte.hu>
2015-03-09 17:26:16 -0400

    Import and export factories added to installer.

--- dll/Jhu.SkyQuery/Install/SkyQueryInstaller.cs
+++ dll/Jhu.SkyQuery/Install/SkyQueryInstaller.cs
@@ -29,6 +29,8 @@ namespace Jhu.SkyQuery.Install
             Federation.QueryFactory = GetUnversionedTypeName(typeof(Jhu.SkyQuery.Jobs.Query.XMatchQueryFactory));
             Federation.FileFormatFactory = GetUnversionedTypeName(typeof(Jhu.SkyQuery.Format.SkyQueryFileFormatFactory));
             Federation.StreamFactory = GetUnversionedTypeName(typeof(Jhu.SkyQuery.IO.SkyQueryStreamFactory));
+            Federation.ImportTablesJobFactory = GetUnversionedTypeName(typeof(Jhu.Graywulf.SciDrive.SciDriveImportTablesJobFactory));
+            Federation.ExportTablesJobFactory = GetUnversionedTypeName(typeof(Jhu.Graywulf.SciDrive.SciDriveExportTablesJobFactory));
 
             Federation.ShortTitle = "SkyQuery";
             Federation.LongTitle = "SkyQuery Astronomical Cross-Match Engine";
@@ -48,87 +50,5 @@ namespace Jhu.SkyQuery.Install
             var jdi = new XMatchQueryJobInstaller(Federation);
             jdi.Install();
         }
-
-#if false   // TODO: old code, delete
-
-            // ---
-            var ef = new EntityFactory(Context);
-
-            var clusterName = domain.Cluster.Name;
-            var domainName = domain.Name;
-
-            // Load default settings
-            // --- Temp database version
-            var tempDatabaseVersion = ef.LoadEntity<DatabaseVersion>(clusterName, Constants.TempDbName, Constants.TempDbName);
-
-            // --- Controller machine
-            var controllerMachine = ef.LoadEntity<Machine>(clusterName, Constants.ControllerMachineRoleName, Constants.ControllerMachineName);
-
-            // --- Schema source server
-            var schemaSourceServerInstance = ef.LoadEntity<ServerInstance>(clusterName, Constants.ControllerMachineRoleName, Constants.ControllerMachineName, Constants.ServerInstanceName);
-
-            var federation = new Federation(domain)
-            {
-                QueryFactory = GetUnversionedTypeName(typeof(Jhu.SkyQuery.Jobs.Query.XMatchQueryFactory)),
-                FileFormatFactory = GetUnversionedTypeName(typeof(Jhu.SkyQuery.Format.SkyQueryFileFormatFactory)),
-                StreamFactory = GetUnversionedTypeName(typeof(Jhu.SkyQuery.IO.SkyQueryStreamFactory)),
-                Name = "SkyQuery",
-                System = true,
-                ShortTitle = "SkyQuery",
-                LongTitle = "SkyQuery Astronomical Cross-Match Engine",
-                Email = "admin@skyquery.net",
-                Copyright = Copyright.InfoCopyright,
-                TempDatabaseVersion = tempDatabaseVersion,
-                ControllerMachine = controllerMachine,
-                SchemaSourceServerInstance = schemaSourceServerInstance,
-
-                //CodeDatabaseVersion
-                //Disclaimer
-                //MyDBDatabaseVersion
-                
-            };
-            federation.Save();
-
-            var fi = new FederationInstaller(federation);
-            fi.GenerateDefaultChildren(myDBServerVersion, codeDatabaseServerVersion);
-
-            // XMatch job
-            var jd = new JobDefinition(federation)
-            {
-                Name = typeof(Jhu.SkyQuery.Jobs.Query.XMatchQueryJob).Name,
-                System = true,
-                WorkflowTypeName = GetUnversionedTypeName(typeof(Jhu.SkyQuery.Jobs.Query.XMatchQueryJob)),
-                Settings = new SqlQueryJobSettings()
-                {
-                    HotDatabaseVersionName = Constants.HotDatabaseVersionName,
-                    StatDatabaseVersionName = Constants.StatDatabaseVersionName,
-                    DefaultDatasetName = Constants.UserDbName,
-                    DefaultSchemaName = Constants.DefaultSchemaName,
-                    QueryTimeout = 7200,
-                },
-            };
-            jd.DiscoverWorkflowParameters();
-            jd.Save();
-
-            var codedd = new DatabaseDefinition(federation)
-            {
-                Name = Constants.CodeDbName,
-                System = true,
-                DatabaseInstanceNamePattern = Constants.CodeDbInstanceNamePattern,
-                DatabaseNamePattern = Constants.CodeDbNamePattern,
-                LayoutType = DatabaseLayoutType.Monolithic,
-            };
-            codedd.Save();
-
-            var codeddi = new DatabaseDefinitionInstaller(codedd);
-            codeddi.GenerateDefaultChildren(codeDatabaseServerVersion, Constants.CodeDbName);
-
-            codedd.LoadDatabaseVersions(true);
-            federation.CodeDatabaseVersion = codedd.DatabaseVersions[Constants.CodeDbName];
-
-            federation.Save();
-        }
-
-#endif
     }
 }
