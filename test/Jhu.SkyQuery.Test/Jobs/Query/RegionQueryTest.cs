075d223187000e61797a00838f034f631e75b2d3
Laszlo Dobos <dobos@complex.elte.hu>
2015-07-31 12:32:27 -0400

    Placeholders for region query added.

---
+++ test/Jhu.SkyQuery.Test/Jobs/Query/RegionQueryTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jhu.Graywulf.Scheduler;
+using Jhu.Graywulf.RemoteService;
+using Jhu.Graywulf.Registry;
+using Jhu.SkyQuery.Jobs.Query;
+
+namespace Jhu.SkyQuery.Jobs.Query.Test
+{
+    [TestClass]
+    public class RegionQueryTest : XMatchQueryTestBase
+    {
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            using (SchedulerTester.Instance.GetExclusiveToken())
+            {
+                PurgeTestJobs();
+            }
+        }
+
+        [ClassCleanup]
+        public static void CleanUp()
+        {
+            using (SchedulerTester.Instance.GetExclusiveToken())
+            {
+                if (SchedulerTester.Instance.IsRunning)
+                {
+                    SchedulerTester.Instance.DrainStop();
+                }
+
+                PurgeTestJobs();
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Query")]
+        public void SimpleRegionQueryTest()
+        {
+            using (SchedulerTester.Instance.GetToken())
+            {
+                var table = GetTestUniqueName();
+                DropUserDatabaseTable(table);
+
+                SchedulerTester.Instance.EnsureRunning();
+                using (RemoteServiceTester.Instance.GetToken())
+                {
+                    RemoteServiceTester.Instance.EnsureRunning();
+
+                    var sql = "SELECT TOP 10 objid, ra, dec INTO " + table + " FROM SDSSDR7:PhotoObj";
+
+                    var guid = ScheduleQueryJob(sql, QueueType.Long);
+
+                    FinishQueryJob(guid);
+                }
+            }
+        }
+    }
+}
