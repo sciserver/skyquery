523d597c00d2ffa402800950789193647cf657da
Laszlo Dobos <dobos@complex.elte.hu>
2015-08-12 10:40:39 -0400

    Test SkyNode script expanded.

--- sql/SkyNodes/TEST/Sample_SDSS.sql
+++ sql/SkyNodes/TEST/Sample_SDSS.sql
@@ -185,12 +185,12 @@ GO
 
 ---------------------------------------------------------------
 
-IF (OBJECT_ID('[dbo].[SDSSDR7PhotoObjAll]') IS NOT NULL)
-DROP TABLE [dbo].[SDSSDR7PhotoObjAll]
+IF (OBJECT_ID('[dbo].[SDSSDR7PhotoObjAll_NoHTM]') IS NOT NULL)
+DROP TABLE [dbo].[SDSSDR7PhotoObjAll_NoHTM]
 
 GO
 
-CREATE TABLE [dbo].[SDSSDR7PhotoObjAll]
+CREATE TABLE [dbo].[SDSSDR7PhotoObjAll_NoHTM]
 (
 	[objId] [bigint] NOT NULL,
 	[skyVersion] [tinyint] NOT NULL,
@@ -208,7 +208,6 @@ CREATE TABLE [dbo].[SDSSDR7PhotoObjAll]
 	[cx] [float] NOT NULL,
 	[cy] [float] NOT NULL,
 	[cz] [float] NOT NULL,
-	[htmId] [bigint] NOT NULL,
 	[zoneId] [bigint] NOT NULL,
 	[u] [real] NOT NULL,
 	[g] [real] NOT NULL,
@@ -216,7 +215,7 @@ CREATE TABLE [dbo].[SDSSDR7PhotoObjAll]
 	[i] [real] NOT NULL,
 	[z] [real] NOT NULL,
 
-	CONSTRAINT PK_SDSSDR7PhotoObjAll PRIMARY KEY 
+	CONSTRAINT PK_SDSSDR7PhotoObjAll_NoHTM PRIMARY KEY 
 	(
 		objID
 	)
@@ -224,51 +223,13 @@ CREATE TABLE [dbo].[SDSSDR7PhotoObjAll]
 
 GO
 
-IF (OBJECT_ID('[dbo].[SDSSDR7PhotoPrimary]') IS NOT NULL)
-DROP VIEW [dbo].[SDSSDR7PhotoPrimary]
-
-GO
-
-CREATE VIEW [dbo].[SDSSDR7PhotoPrimary]
-AS
-SELECT * FROM [SDSSDR7PhotoObjAll] WITH(NOLOCK)
-    WHERE mode=1
-
-GO
-
-IF (OBJECT_ID('[dbo].[SDSSDR7Star]') IS NOT NULL)
-DROP VIEW [dbo].[SDSSDR7Star]
-
-GO
-
-CREATE VIEW [dbo].[SDSSDR7Star]
-AS
-SELECT * 
-    FROM [SDSSDR7PhotoPrimary]
-    WHERE type = 6
-
-GO
-
-IF (OBJECT_ID('[dbo].[SDSSDR7Galaxy]') IS NOT NULL)
-DROP VIEW [dbo].[SDSSDR7Galaxy]
-
-GO
-
-CREATE VIEW [dbo].[SDSSDR7Galaxy]
-AS
-SELECT * 
-    FROM [SDSSDR7PhotoPrimary]
-    WHERE type = 3
-
-GO
-
 -- Populate
 
 DECLARE @H float = 4.0 / 3600.0
 
-INSERT [dbo].[SDSSDR7PhotoObjAll] WITH (TABLOCKX)
+INSERT [dbo].[SDSSDR7PhotoObjAll_NoHTM] WITH (TABLOCKX)
 SELECT [objId], [skyVersion], [run], [rerun], [camcol], [field], [obj], [mode], [type],
-	[ra], [dec], [raErr], [decErr], [cx], [cy], [cz], [htmId], 
+	[ra], [dec], [raErr], [decErr], [cx], [cy], [cz],
 	CONVERT(INT,FLOOR((dec + 90.0) / @H)) AS [zoneID],
 	p.[u], p.[g], p.[r], p.[i], [z]
 FROM [SkyNode_SDSSDR7]..[PhotoObjAll] p
@@ -277,17 +238,9 @@ INNER JOIN SkyQuery_Code.htm.CoverCircleEq(0, 0, 240) htm
 
 -- Create indexes
 
-CREATE INDEX [IX_SDSSDR7PhotoObjAll_HtmID] ON [SDSSDR7PhotoObjAll]
-(
-	htmID
-)
-INCLUDE
-(
-	[mode], [type],
-	[ra], [dec], [cx], [cy], [cz]
-)
 
-CREATE INDEX [IX_SDSSDR7PhotoObjAll_ZoneID] ON [SDSSDR7PhotoObjAll]
+
+CREATE INDEX [IX_SDSSDR7PhotoObjAll_NoHTM_ZoneID] ON [SDSSDR7PhotoObjAll_NoHTM]
 (
 	zoneID, ra
 )
