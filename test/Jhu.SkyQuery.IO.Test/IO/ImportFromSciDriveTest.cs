4feb38b1804cf7a4d2cde690a226fda9fab58a73
Laszlo Dobos <dobos@complex.elte.hu>
2016-06-11 17:30:45 -0400

    SciDrive import test added.

---
+++ test/Jhu.SkyQuery.IO.Test/IO/ImportFromSciDriveTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jhu.SkyQuery.IO.Test.IO
+{
+    [TestClass]
+    public class ImportFromSciDriveTest : Jhu.Graywulf.SciDrive.ImportTest
+    {
+        [TestMethod]
+        public void ImportSciDriveMyCatalogCsvTest()
+        {
+            ImportFileHelper("skyquery_io_test/mycatalog.txt", "ImportMyCatalogTxtTest");
+        }
+
+#if false
+        [TestMethod]
+        public void ImportCompressedFromSciDriveTest()
+        {
+            ImportFileHelper("graywulf_io_test/csv_numbers.csv.gz", "ImportCompressedFromSciDriveTest");
+        }
+
+        [TestMethod]
+        public void ImportArchiveFromSciDriveTest()
+        {
+            ImportFileHelper("graywulf_io_test/csv_numbers.zip", "ImportArchiveFromSciDriveTest");
+        }
+#endif
+    }
+}
