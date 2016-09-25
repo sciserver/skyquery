512585d823037a79b20fc3ba29a7501d71329435
Laszlo Dobos <dobos@complex.elte.hu>
2016-09-25 15:31:56 -0400

    Implemented checks.

---
+++ web/Jhu.SkyQuery.Web.UI/Check.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Check.aspx.cs" Inherits="Jhu.SkyQuery.Web.UI.Check" %>
+
+<html>
+<head runat="server">
+</head>
+<body>
+</body>
+</html>
---
+++ web/Jhu.SkyQuery.Web.UI/Check.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Jhu.SkyQuery.Web.UI
+{
+    public partial class Check : Jhu.Graywulf.Web.UI.Check
+    {
+    }
+}
