e7a927fbf2dfe6d5317a246025137a85f837f566
Laszlo Dobos <dobos@complex.elte.hu>
2016-10-31 18:14:20 -0400

    Upgraded to new config util.

---
+++ exe/Jhu.SkyQuery.Install.CmdLineUtil/post-build.ps1
@@ -0,0 +1,12 @@
+﻿param(
+	[string]$SolutionDir,
+	[string]$SolutionName,
+	[string]$ProjectDir,
+	[string]$ProjectName,
+	[string]$OutDir,
+	[string]$TargetName
+)
+
+cp $ProjectDir$OutDir$TargetName.exe $SolutionDir$OutDir
+cp $ProjectDir$OutDir$TargetName.pdb $SolutionDir$OutDir
+cp $ProjectDir$OutDir$TargetName.exe.config $SolutionDir$OutDir
