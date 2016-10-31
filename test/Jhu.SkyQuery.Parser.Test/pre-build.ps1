ea26cb7ae5a93f1877481df794183b790c189535
Laszlo Dobos <dobos@complex.elte.hu>
2016-10-31 17:33:41 -0400

    Updated to new config util.

---
+++ test/Jhu.SkyQuery.Parser.Test/pre-build.ps1
@@ -0,0 +1,10 @@
+﻿param(
+	[string]$SolutionDir,
+	[string]$SolutionName,
+	[string]$ProjectDir,
+	[string]$ProjectName,
+	[string]$OutDir,
+	[string]$TargetName
+)
+
+& "${SolutionDir}${OutDir}gwconfig.exe" merge $SolutionDir$SolutionName.sln $ProjectName
