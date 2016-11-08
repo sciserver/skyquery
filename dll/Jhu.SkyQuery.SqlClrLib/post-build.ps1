e53d2df4add91b7a0816a07b7a35949be1878035
Laszlo Dobos <dobos@complex.elte.hu>
2016-11-08 14:57:16 -0500

    Generate SQL scripts on post-build.

--- dll/Jhu.SkyQuery.SqlClrLib/post-build.ps1
+++ dll/Jhu.SkyQuery.SqlClrLib/post-build.ps1
@@ -7,5 +7,9 @@
 	[string]$TargetName
 )
 
+& "${SolutionDir}${OutDir}gwsqlclrutil.exe" $ProjectDir$OutDir$TargetName.dll Unrestricted
+
 cp $ProjectDir$OutDir$TargetName.dll $SolutionDir$OutDir
 cp $ProjectDir$OutDir$TargetName.pdb $SolutionDir$OutDir
+cp $ProjectDir$OutDir$TargetName.Create.sql $SolutionDir$OutDir
+cp $ProjectDir$OutDir$TargetName.Drop.sql $SolutionDir$OutDir
