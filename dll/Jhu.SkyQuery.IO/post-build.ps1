51e442cbf234f2946eb1c89e97eded040c13a3b0
Laszlo Dobos <dobos@complex.elte.hu>
2016-11-21 19:47:15 -0500

    Copy missing dll to output dir.

--- dll/Jhu.SkyQuery.IO/post-build.ps1
+++ dll/Jhu.SkyQuery.IO/post-build.ps1
@@ -9,3 +9,5 @@
 
 cp $ProjectDir$OutDir$TargetName.dll $SolutionDir$OutDir
 cp $ProjectDir$OutDir$TargetName.pdb $SolutionDir$OutDir
+cp ${ProjectDir}${OutDir}Jhu.SharpFitsIO.dll $SolutionDir$OutDir
+cp ${ProjectDir}${OutDir}Jhu.SharpFitsIO.pdb $SolutionDir$OutDir
