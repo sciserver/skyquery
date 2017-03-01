00addddc39e6234e3e5b2bb66cfc0fc199a16c45
Laszlo Dobos <dobos@complex.elte.hu>
2017-03-01 11:34:37 -0500

    Fixed unit tests.

--- test/Jhu.SkyQuery.IO.Test/IO/ImportFromSciDriveTest.cs
+++ test/Jhu.SkyQuery.IO.Test/IO/ImportFromSciDriveTest.cs
@@ -6,6 +6,18 @@ namespace Jhu.SkyQuery.IO.Test.IO
     [TestClass]
     public class ImportFromSciDriveTest : Jhu.Graywulf.SciDrive.ImportTest
     {
+        [ClassInitialize]
+        public new static void Initialize(TestContext context)
+        {
+            Jhu.Graywulf.Web.Api.V1.ImportTest.Initialize(context);
+        }
+
+        [ClassCleanup]
+        public new static void CleanUp()
+        {
+            Jhu.Graywulf.Web.Api.V1.ImportTest.CleanUp();
+        }
+
         [TestMethod]
         public void ImportSciDriveMyCatalogCsvTest()
         {
