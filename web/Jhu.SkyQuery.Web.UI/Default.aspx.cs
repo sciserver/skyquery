45dc4c2c89ac6155fcebdd85ea8942fd24ab15bc
Laszlo Dobos <dobos@complex.elte.hu>
2017-07-15 14:44:10 -0400

    Always use PageBase to enable logging.

--- web/Jhu.SkyQuery.Web.UI/Default.aspx.cs
+++ web/Jhu.SkyQuery.Web.UI/Default.aspx.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Jhu.Graywulf.Web.UI;
 
 namespace Jhu.SkyQuery.Web.UI
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class Default : PageBase
     {
         public static string GetUrl()
         {
