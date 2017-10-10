6a1633a8ebf3ea082cd90b8e40663ac40477a949
Laszlo Dobos <dobos@complex.elte.hu>
2017-10-10 05:11:30 -0400

    Improvements to table export UI: * automatically change file extensions * sorted out many issues with URI handling * support special characters in table names

---
+++ test/Jhu.SkyQuery.IO.Test/Format/Fits/ExportFitsTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jhu.Graywulf.IO;
+using Jhu.Graywulf.Format;
+using Jhu.Graywulf.Test;
+using Jhu.Graywulf.Schema;
+using Jhu.Graywulf.Data;
+using Jhu.SkyQuery.Format.Fits;
+
+namespace Jhu.SkyQuery.Format.Fits
+{
+    [TestClass]
+    public class ExportFitsTest : TestClassBase
+    {
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            StartLogger();
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            StopLogger();
+        }
+        
+        [TestMethod]
+        public void EmptyTableTest()
+        {
+            var ms = new MemoryStream();
+
+            using (var cn = IOTestDataset.OpenConnection())
+            {
+                using (var cmd = new SmartCommand(IOTestDataset, cn.CreateCommand()))
+                {
+                    cmd.CommandText = "SELECT * FROM EmptyTable";
+
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        var fits = new FitsFileWrapper(ms, DataFileMode.Write);
+                        fits.WriteFromDataReader(dr);
+                    }
+                }
+            }
+
+        }
+        
+    }
+}
