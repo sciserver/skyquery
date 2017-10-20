b5d6643d4e3f4caa74fbec73eafe200516aa9ad0
Laszlo Dobos <dobos@complex.elte.hu>
2017-10-20 08:51:21 +0200

    Added TAP client skeleton.

---
+++ dll/Jhu.SkyQuery.Tap/Tap/Client/TapCommand.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+
+namespace Jhu.SkyQuery.Tap.Client
+{
+    public class TapCommand : DbCommand
+    {
+        public override string CommandText
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int CommandTimeout
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override CommandType CommandType
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool DesignTimeVisible
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override UpdateRowSource UpdatedRowSource
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected override DbConnection DbConnection
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected override DbParameterCollection DbParameterCollection
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected override DbTransaction DbTransaction
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ExecuteNonQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ExecuteScalar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Prepare()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DbParameter CreateDbParameter()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DbDataReader ExecuteDbDataReader(CommandBehavior behavior)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dll/Jhu.SkyQuery.Tap/Tap/Client/TapConnection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+
+namespace Jhu.SkyQuery.Tap.Client
+{
+    public class TapConnection : DbConnection
+    {
+        public override string ConnectionString
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string Database
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string DataSource
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string ServerVersion
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override ConnectionState State
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void ChangeDatabase(string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DbCommand CreateDbCommand()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
